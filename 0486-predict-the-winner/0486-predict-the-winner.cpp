7b9e54e9b6f72734e94d38af52971a7334ea8f88
Lokesh-7630 <chittelokesh647@gmail.com>
2026-08-01 17:40:27 +0530

    Time: 3 ms (39.21%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<int>& nums, vector<vector<int>>&dp){
-        if(i==j) return nums[i];
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int takeleft=nums[i]-solve(i+1,j,nums,dp);
-        int takeright=nums[j]-solve(i,j-1,nums,dp);
-        dp[i][j]= max(takeleft,takeright);
-        return dp[i][j];
-    }
+    
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans=solve(0,n-1,nums,dp);
-        if(ans>=0){
-            return true;
-        }else{
-            return false;
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0; i<n; i++){
+            dp[i][i]=nums[i];
         }
+        for(int len=2; len<=n; len++){
+            for(int i=0; i<=n-len; i++){
+               int j=i+len-1;
+            int takeleft=nums[i]-dp[i+1][j];
+            int takeright=nums[j]-dp[i][j-1]; 
+            dp[i][j]=max(takeleft,takeright);
+            
+            }
+        }
+        return dp[0][n-1]>=0;
     }
 };
