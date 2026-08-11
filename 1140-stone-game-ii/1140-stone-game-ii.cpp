d0def2eee06892140faac7c848e302528328c8c4
Lokesh-7630 <chittelokesh647@gmail.com>
2026-08-11 21:57:32 +0530

    Time: 5 ms (80.39%), Space: 13.9 MB (29.38%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>suffix(n+1,0);
+        for(int i=n-1; i>=0; i--){
+            suffix[i]=suffix[i+1]+piles[i];
+        }
+        for(int i=n-1; i>=0; i--){
+            for(int m=1; m<=n; m++){
+               if(i+2*m>=n){
+                dp[i][m]=suffix[i];
+               }else{
+                for(int x=1; x<=2*m; x++){
+                    int nextM=max(m,x);
+                  dp[i][m]=max(dp[i][m],suffix[i]-dp[i+x][nextM]);
+                }
+               }
+            }
+        }
+        return dp[0][1];
+    }
+};
